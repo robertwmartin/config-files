da6cc229bf5904ef88051011588ff15b44695b2d
Robert Martin <robert@robertwmartin.com>
2025-04-18 15:23:18 -0600

    Fixed PATH to have .local/bin and bin

--- .config/zsh/00-environment.zsh
+++ .config/zsh/00-environment.zsh
@@ -3,7 +3,7 @@ export TERMINAL_FONT="JetBrainsMono Nerd Font"
 
 # Environment
 export EDITOR="vim"
-export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/.local/bin:$HOME/bin:$PATH"
 
 # Enable colors in manual pages
 export LESS='-R'

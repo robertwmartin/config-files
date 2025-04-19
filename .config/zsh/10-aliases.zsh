85a98421a025b9f163fe58c08e43639e7d11ff98
Robert Martin <robert@robertwmartin.com>
2025-04-18 22:07:28 -0600

    Final edits for Nord config in kitty and lsd

--- .config/zsh/10-aliases.zsh
+++ .config/zsh/10-aliases.zsh
@@ -1,6 +1,6 @@
 # Aliases
 alias ls='lsd -l --group-dirs=first --icon=auto'
-alias ll='lsd -l --group-dirs=first --icon=auto'
+alias ll='lsd -lha --group-dirs=first --icon=auto'
 alias la='lsd -la --group-dirs=first --icon=auto'
 
 alias ..='cd ..'

0c0ca95bb7cff2a41d07e005c25ca4e4bf2fcd13
Robert Martin <robert@robertwmartin.com>
2025-04-19 12:41:18 -0600

    Added color support for text typed on command line and added zsh-syntax-highlighting support

--- .config/zsh/00-environment.zsh
+++ .config/zsh/00-environment.zsh
@@ -11,3 +11,10 @@ export LESS='-R'
 # Hardcoded Nord terminal colors (replaces broken nord-dircolors file)
 export LS_COLORS='di=0;38;5;110:ln=0;38;5;109:fi=0;38;5;245:*.sh=1;38;5;108:*.md=38;5;180:*.zip=38;5;203:*.tar=38;5;203:*.gz=38;5;203:*.jpg=38;5;217:*.png=38;5;217'
 
+# Enable green-screen style syntax highlighting
+source ~/.config/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# Make all typed input green
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main)
+ZSH_HIGHLIGHT_STYLES[default]='fg=green'
+

99d12bddded390374ae1886df06296dbdcb82d98
Robert Martin <robert@robertwmartin.com>
2025-04-18 21:16:15 -0600

    Fix terminal color theming: hardcode LS_COLORS for Nord palette

--- .config/zsh/00-environment.zsh
+++ .config/zsh/00-environment.zsh
@@ -8,8 +8,6 @@ export PATH="$HOME/.local/bin:$HOME/bin:$PATH"
 # Enable colors in manual pages
 export LESS='-R'
 
-# Nord terminal colors (used by ls, eza, etc.)
-if [ -f "$HOME/.dircolors" ]; then
-  eval "$(dircolors -b $HOME/.dircolors)"
-fi
+# Hardcoded Nord terminal colors (replaces broken nord-dircolors file)
+export LS_COLORS='di=0;38;5;110:ln=0;38;5;109:fi=0;38;5;245:*.sh=1;38;5;108:*.md=38;5;180:*.zip=38;5;203:*.tar=38;5;203:*.gz=38;5;203:*.jpg=38;5;217:*.png=38;5;217'
 

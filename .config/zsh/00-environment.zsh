52314221ad3f777c95eb03c80731ba2f3dd45f87
Robert Martin <robert@robertwmartin.com>
2025-04-18 20:51:10 -0600

    Add Nord dircolors for consistent terminal theming

--- .config/zsh/00-environment.zsh
+++ .config/zsh/00-environment.zsh
@@ -8,3 +8,8 @@ export PATH="$HOME/.local/bin:$HOME/bin:$PATH"
 # Enable colors in manual pages
 export LESS='-R'
 
+# Nord terminal colors (used by ls, eza, etc.)
+if [ -f "$HOME/.dircolors" ]; then
+  eval "$(dircolors -b $HOME/.dircolors)"
+fi
+

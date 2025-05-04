479e5b6a1f0711b7d4f07708c0c3b0db6ac8a9e5
Robert Martin <robert@robertwmartin.com>
2025-05-04 16:15:33 -0600

    Refine GNOME keybinding script; Meta+N issues under review

--- bin/set-keybindings.sh
+++ bin/set-keybindings.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-# Set GNOME keybinding for system notifications to Meta+N
 
-gsettings set org.gnome.settings-daemon.plugins.media-keys show-notifications '<Super>n'
+# Check for graphical session
+if [ -z "$DISPLAY" ]; then
+  echo "No graphical session detected. Skipping keybinding setup."
+  exit 0
+fi
 
-# Optional: Unbind existing shortcut (if known)
-# gsettings set org.gnome.settings-daemon.plugins.media-keys show-notifications '@as []'
+# Define path
+KEYBINDING_PATH="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+
+# Set the keybinding
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['$KEYBINDING_PATH']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$KEYBINDING_PATH name "Show Notifications"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$KEYBINDING_PATH command "gnome-shell --show-notification-log"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$KEYBINDING_PATH binding "<Super>n"
 
